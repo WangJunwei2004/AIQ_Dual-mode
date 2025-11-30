c6179136efab82a0dfc77dbf09854a375d0b5628
JayWang <8835139@gmail.com>
2025-11-30 23:27:25 +0800

    Update server.js

--- server.js
+++ server.js
@@ -252,6 +252,13 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.static(path.join(__dirname)));
 
+app.get('/api/model-config', (req, res) => {
+    res.json({
+        localModel: LOCAL_MODEL,
+        checklistModel: LOCAL_CHECKLIST_MODEL
+    });
+});
+
 app.post('/api/prompt', async (req, res) => {
     try {
         const { provider, typeName, checklistText } = req.body || {};
